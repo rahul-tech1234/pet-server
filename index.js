f2bd2d1540cf1ea0da53649bb2c46a3cc582896a
rahul-tech1234 <rahul.laravel.dev@gmail.com>
2026-06-28 01:38:50 +0600

    PostData to mongodb  form addpet page

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+dotenv.config();
+const uri = process.env.MONGODB_URL;
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const db = client.db("pet-plutform");
+        const petCollection = db.collection("pet");
+        app.post("/pet", async (req, res) => {
+            const petData = req.body;
+            console.log(petData);
+            const result = await petCollection.insertOne(petData);
+            res.json(result);
+        });
+
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!",
+        );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
